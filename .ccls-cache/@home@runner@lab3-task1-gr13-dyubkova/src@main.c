dea7a533fe28530af43edb0a5e467306d1a8eab9
enotika <91068026+enotika@users.noreply.github.com>
2023-02-28 13:11:38 +0000

    added function that calculate count of letters in sentences and main body of main.c.

--- .ccls-cache/@home@runner@lab3-task1-gr13-dyubkova/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr13-dyubkova/src@main.c
@@ -1,8 +1,31 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
+
+int count(char* str, char b){
+  int ans = 0;
+  for(int i = 0; i < strlen(str); i++){
+    if(str[i] == b){
+      ans++;
+    }
+  }
+  return ans;
+}
 
 void main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	char * str1 = NULL;
+  char * str2 = NULL;
+  char * str3 = NULL;
+  size_t inputSize;
+  char sym;
+  printf("Enter 3 sentences:\n");
+  getline(&str1, &inputSize, stdin);
+  getline(&str2, &inputSize, stdin);
+  getline(&str3, &inputSize, stdin);
+  printf("enter your letter:\n");
+  scanf("%c", &sym);
+  int ans = count(str1, sym);
+  ans += count(str2, sym);
+  ans += count(str3, sym);
+  printf("Count of your letter in first sentence is: %d\n", ans);
 }
